3c6dcbabef195e9dfd16eae7329191ac47432ba6
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-02-16 14:27:13 +0100

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { Hono } from 'hono';
 import { prettyJSON } from 'hono/pretty-json';
 import { Env } from './types';
 import { bearerAuth } from 'hono/bearer-auth';
+import { cors } from 'hono/cors';
 
 export const app = new Hono<{ Bindings: Env }>();
 
+app.use(cors());
 app.use('/admin/*', async (c, next) => {
 	const token = c.env.BEARER_TOKEN;
 	const bearer = bearerAuth({ token });
